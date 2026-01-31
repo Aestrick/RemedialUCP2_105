c0ca12384bd16831bd8047f833f3a67669ad51f6
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-01-31 09:59:46 +0700

    mengisi kodingan ContainerApp.kt dan LocalRepositoryPerpus.kt

--- app/src/main/java/com/example/ucp2remidipam/repository/LocalRepositoryPerpus.kt
+++ app/src/main/java/com/example/ucp2remidipam/repository/LocalRepositoryPerpus.kt
@@ -1,2 +1,48 @@
 package com.example.ucp2remidipam.repository
 
+import com.example.ucp2remidipam.data.PerpusDao
+import com.example.ucp2remidipam.data.entity.Buku
+import com.example.ucp2remidipam.data.entity.Kategori
+import com.example.ucp2remidipam.data.entity.Pengarang
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryPerpus(
+    private val perpusDao: PerpusDao
+) : RepositoryPerpus {
+
+    override suspend fun insertKategori(kategori: Kategori) {
+        perpusDao.insertKategori(kategori)
+    }
+
+    override fun getAllKategori(): Flow<List<Kategori>> {
+        return perpusDao.getAllKategori()
+    }
+
+    override suspend fun insertBuku(buku: Buku) {
+        perpusDao.insertBuku(buku)
+    }
+
+    override fun getAllBuku(): Flow<List<Buku>> {
+        return perpusDao.getAllBuku()
+    }
+
+    override fun getBuku(id: Int): Flow<Buku> {
+        return perpusDao.getBuku(id)
+    }
+
+    override suspend fun updateBuku(buku: Buku) {
+        perpusDao.updateBuku(buku)
+    }
+
+    override suspend fun deleteBuku(buku: Buku) {
+        perpusDao.deleteBukuSoft(buku.id)
+    }
+
+    override suspend fun insertPengarang(pengarang: Pengarang) {
+        perpusDao.insertPengarang(pengarang)
+    }
+
+    override fun getAllPengarang(): Flow<List<Pengarang>> {
+        return perpusDao.getAllPengarang()
+    }
+}
