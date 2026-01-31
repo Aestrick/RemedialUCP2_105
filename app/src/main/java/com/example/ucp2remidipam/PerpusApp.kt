203c401b2b8baf9e5cf941d13f46c5ceceb34e7f
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-01-31 10:12:57 +0700

    ngisi file PerpusApp.kt sama RepositoryPerpus.kt

--- app/src/main/java/com/example/ucp2remidipam/PerpusApp.kt
+++ app/src/main/java/com/example/ucp2remidipam/PerpusApp.kt
@@ -1,2 +1,14 @@
 package com.example.ucp2remidipam
 
+import android.app.Application
+import com.example.ucp2remidipam.repository.AppContainer
+import com.example.ucp2remidipam.repository.ContainerApp
+
+class PerpusApp : Application() {
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerApp(this)
+    }
+}
--- app/src/main/java/com/example/ucp2remidipam/repository/RepositoryPerpus.kt
+++ app/src/main/java/com/example/ucp2remidipam/repository/RepositoryPerpus.kt
@@ -1,2 +1,18 @@
 package com.example.ucp2remidipam.repository
 
+import com.example.ucp2remidipam.data.entity.Buku
+import com.example.ucp2remidipam.data.entity.Kategori
+import com.example.ucp2remidipam.data.entity.Pengarang
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryPerpus {
+    suspend fun insertKategori(kategori: Kategori)
+    fun getAllKategori(): Flow<List<Kategori>>
+    suspend fun insertBuku(buku: Buku)
+    fun getAllBuku(): Flow<List<Buku>>
+    fun getBuku(id: Int): Flow<Buku>
+    suspend fun updateBuku(buku: Buku)
+    suspend fun deleteBuku(buku: Buku)
+    suspend fun insertPengarang(pengarang: Pengarang)
+    fun getAllPengarang(): Flow<List<Pengarang>>
+}
