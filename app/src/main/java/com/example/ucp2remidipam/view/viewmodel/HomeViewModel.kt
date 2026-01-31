1ee64d4ddbb38aa0cd62762e0228509169c50bb8
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-01-31 10:08:31 +0700

    nambahin file HomeViewModel.kt dan PenyediaViewModel.kt di package viewmodel

---
+++ app/src/main/java/com/example/ucp2remidipam/view/viewmodel/HomeViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.ucp2remidipam.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2remidipam.data.entity.Buku
+import com.example.ucp2remidipam.repository.RepositoryPerpus
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+data class HomeUiState(
+    val listBuku: List<Buku> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
+
+class HomeViewModel(
+    private val repositoryPerpus: RepositoryPerpus
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryPerpus.getAllBuku()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listBuku = it,
+                isLoading = false
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(isLoading = true)
+        )
+}
---
+++ app/src/main/java/com/example/ucp2remidipam/view/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.ucp2remidipam.view.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2remidipam.PerpusApp
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiPerpus().container.repositoryPerpus
+            )
+        }
+    }
+}
+
+fun CreationExtras.aplikasiPerpus(): PerpusApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PerpusApp)
