2e345dc112488741aaf3fef11beb8fc681114dcc
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-01-31 09:59:36 +0700

    mengisi kodingan ContainerApp.kt dan LocalRepositoryPerpus.kt

--- app/src/main/java/com/example/ucp2remidipam/repository/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2remidipam/repository/ContainerApp.kt
@@ -1,2 +1,14 @@
 package com.example.ucp2remidipam.repository
 
+import android.content.Context
+import com.example.ucp2remidipam.data.PerpusDatabase
+
+interface AppContainer {
+    val repositoryPerpus: RepositoryPerpus
+}
+
+class ContainerApp(private val context: Context) : AppContainer {
+    override val repositoryPerpus: RepositoryPerpus by lazy {
+        LocalRepositoryPerpus(PerpusDatabase.getDatabase(context).perpusDao())
+    }
+}
